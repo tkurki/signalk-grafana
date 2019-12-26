9f84d388fd4c8e904797bfe2ea9f4753aa6a04d5
Teppo Kurki <teppo.kurki@iki.fi>
2019-12-26 19:58:56 +0200

    feature: support multiple metrics etc

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -3,34 +3,53 @@ import { DataQueryRequest, DataQueryResponse, DataSourceApi, DataSourceInstanceS
 import { SignalKQuery, SignalKDataSourceOptions } from './types';
 import { Observable } from 'rxjs';
 
+interface PathValue {
+  path: string
+  value: number
+}
+
 export class DataSource extends DataSourceApi<SignalKQuery, SignalKDataSourceOptions> {
-  hostname: string
+  hostname: string;
   constructor(instanceSettings: DataSourceInstanceSettings<SignalKDataSourceOptions>) {
     super(instanceSettings);
-    this.hostname = instanceSettings.jsonData.hostname || ''
+    this.hostname = instanceSettings.jsonData.hostname || '';
   }
 
   query(options: DataQueryRequest<SignalKQuery>): Observable<DataQueryResponse> {
-    console.log(options);
 
     return new Observable<DataQueryResponse>(subscriber => {
-      const streamId = `stream-${options.requestId}`;
       const maxDataPoints = options.maxDataPoints || 1000;
 
-      const data = new CircularDataFrame({
-        append: 'tail',
-        capacity: maxDataPoints,
+      const pathValueHandlers: ((pv:PathValue) => void)[] = options.targets.map((target, i) => {
+        const data = new CircularDataFrame({
+          append: 'tail',
+          capacity: maxDataPoints,
+        });
+
+        data.refId = target.refId;
+        data.name = target.path;
+        data.addField({ name: 'time', type: FieldType.time });
+        data.addField({ name: target.path, type: FieldType.number });
+
+        const addNextRow = (value: number, time: number) => {
+          data.fields[0].values.add(time);
+          data.fields[1].values.add(value);
+        };
+
+        const pushNextEvent = (value: number) => {
+          addNextRow(value, Date.now());
+          subscriber.next({
+            data: [data],
+            key: target.refId,
+          });
+        };
+
+        return (pathValue: PathValue) => {
+          if (pathValue.path === target.path) {
+            pushNextEvent(pathValue.value, i);
+          }
+        };
       });
-
-      // data.refId = target.refId;
-      // data.name = target.alias || 'Signal ' + target.refId;
-      data.addField({ name: 'time', type: FieldType.time });
-      data.addField({ name: 'value', type: FieldType.number });
-
-      const addNextRow = (value: number, time: number) => {
-        data.fields[0].values.add(time);
-        data.fields[1].values.add(value);
-      };
 
       const ws = new WebSocket(`ws://${this.hostname}/signalk/v1/stream`);
       ws.onmessage = event => {
@@ -39,21 +58,10 @@ export class DataSource extends DataSourceApi<SignalKQuery, SignalKDataSourceOpt
           msg.updates.forEach((update: any) => {
             update.values &&
               update.values.forEach((pathValue: any) => {
-                if (pathValue.path === options.targets[0].path) {
-                  pushNextEvent(pathValue.value);
-                }
+                pathValueHandlers.forEach(pm => pm(pathValue))
               });
           });
       };
-
-      const pushNextEvent = (value: number) => {
-        addNextRow(value, Date.now());
-        subscriber.next({
-          data: [data],
-          key: streamId,
-        });
-      };
-
 
       return () => {
         ws.close();

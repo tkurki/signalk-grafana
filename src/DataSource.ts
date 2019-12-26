ffc16de0c4d0f4a6de37ba96a8786317d864b821
Teppo Kurki <teppo.kurki@iki.fi>
2019-12-26 12:43:00 +0200

    feature: initial streaming datasource impl

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,37 +1,66 @@
-import defaults from 'lodash/defaults';
+import { DataQueryRequest, DataQueryResponse, DataSourceApi, DataSourceInstanceSettings, FieldType, CircularDataFrame } from '@grafana/data';
 
-import { DataQueryRequest, DataQueryResponse, DataSourceApi, DataSourceInstanceSettings, MutableDataFrame, FieldType } from '@grafana/data';
-
-import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
+import { MyQuery, MyDataSourceOptions } from './types';
+import { Observable } from 'rxjs';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
   }
 
-  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-    const { range } = options;
-    const from = range.from.valueOf();
-    const to = range.to.valueOf();
+  query(options: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
+    console.log(options);
 
-    // Return a constant for each query.
-    const data = options.targets.map(target => {
-      const query = defaults(target, defaultQuery);
-      return new MutableDataFrame({
-        refId: query.refId,
-        fields: [
-          { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
-        ],
+    return new Observable<DataQueryResponse>(subscriber => {
+      const streamId = `stream-${options.requestId}`;
+      const maxDataPoints = options.maxDataPoints || 1000;
+
+      const data = new CircularDataFrame({
+        append: 'tail',
+        capacity: maxDataPoints,
       });
-    });
 
-    return { data };
+      // data.refId = target.refId;
+      // data.name = target.alias || 'Signal ' + target.refId;
+      data.addField({ name: 'time', type: FieldType.time });
+      data.addField({ name: 'value', type: FieldType.number });
+
+      const addNextRow = (value: number, time: number) => {
+        data.fields[0].values.add(time);
+        data.fields[1].values.add(value);
+      };
+
+      const ws = new WebSocket('ws://demo.signalk.org/signalk/v1/stream');
+      ws.onmessage = event => {
+        const msg = JSON.parse(event.data);
+        msg.updates &&
+          msg.updates.forEach((update: any) => {
+            update.values &&
+              update.values.forEach((pathValue: any) => {
+                if (pathValue.path === 'navigation.speedOverGround') {
+                  pushNextEvent(pathValue.value);
+                }
+              });
+          });
+      };
+
+      const pushNextEvent = (value: number) => {
+        addNextRow(value, Date.now());
+        subscriber.next({
+          data: [data],
+          key: streamId,
+        });
+      };
+
+
+      return () => {
+        console.log('unsubscribing to stream ' + streamId);
+        ws.close();
+      };
+    });
   }
 
   async testDatasource() {
-    // Implement a health check for your data source.
-
     return {
       status: 'success',
       message: 'Success',

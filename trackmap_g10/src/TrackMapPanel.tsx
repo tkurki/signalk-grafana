b05d510312a3df5673a2186dad74e711fb1b3cfd
Teppo Kurki <teppo.kurki@iki.fi>
2024-04-01 22:56:37 +0300

    port of grafana v9 trackmap

---
+++ trackmap_g10/src/TrackMapPanel.tsx
@@ -0,0 +1,206 @@
+import React, { PureComponent } from 'react';
+import { PanelProps, AbsoluteTimeRange, DataHoverEvent } from '@grafana/data';
+import { TrackMapOptions } from 'types';
+import memoize from 'memoize-one';
+import { interpolateRdYlBu } from 'd3-scale-chromatic';
+
+interface Props extends PanelProps<TrackMapOptions> {}
+
+interface State {
+  currentPoint?: any;
+}
+
+interface PositionWithTime {
+  timestamp: any;
+  position: LatLng;
+  value?: number;
+}
+
+interface MapParams {
+  trackGeojson: LineString;
+  bounds: any;
+  pointsByTime: PositionWithTime[];
+  getTimeframeByBounds: (b: LatLngBounds) => AbsoluteTimeRange | null;
+  hasDataValues: boolean;
+}
+
+import { MapContainer, GeoJSON, TileLayer, CircleMarker, TileLayerProps, Tooltip, Pane, useMapEvent } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import { LineString } from 'geojson';
+import { LatLngBounds, LatLng } from 'leaflet';
+import ValidatingBuffer from 'ValidatingBuffer';
+import { PanelContext, PanelContextRoot } from '@grafana/ui';
+
+export class TrackMapPanel extends PureComponent<Props, State> {
+  static contextType = PanelContextRoot;
+  panelContext: PanelContext = {} as PanelContext;
+
+  cursorSubscription: any; //RxJs Subscription
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  pointByTime(ts: number) {
+    //FIXME dataToMapParams only when data changes
+    return dataToMapParams(this.props.data).pointsByTime.find((pt) => pt.timestamp > ts);
+  }
+
+  componentDidMount() {
+    const panelContext = this.context as PanelContext;
+    const { eventBus } = panelContext;
+
+    this.cursorSubscription = eventBus.getStream(DataHoverEvent).subscribe({
+      next: (evt) => {
+        if (evt.payload.point.time !== null) {
+          const currentPoint = this.pointByTime(evt.payload.point.time);
+          if (currentPoint) {
+            this.setState({ currentPoint: currentPoint.position });
+          }
+        }
+      },
+    });
+  }
+
+  componentWillUnmount() {
+    this.cursorSubscription?.unsubscribe();
+  }
+
+  render() {
+    //FIXME dataToMapParams only when data changes
+    const { bounds, trackGeojson, getTimeframeByBounds, pointsByTime, hasDataValues } = dataToMapParams(
+      this.props.data
+    );
+    const onBoxZoomEnd = ({ boxZoomBounds }: { boxZoomBounds: LatLngBounds }) => {
+      const timeframe = getTimeframeByBounds(boxZoomBounds);
+      if (timeframe) {
+        this.props.onChangeTimeRange(timeframe);
+      }
+    };
+    console.log(onBoxZoomEnd)
+
+    function ZoomendHandler() {
+      const map = useMapEvent('zoomend', (e) => {
+        console.log(map.getBounds())
+        onBoxZoomEnd({boxZoomBounds: map.getBounds()})
+      })
+      return null
+    }
+
+    return (
+      <MapContainer bounds={bounds} style={{ width: '100%', height: '100%' }}>
+        <ZoomendHandler />
+        {this.props.options.layers.map(toTileLayer)}
+        <GeoJSON key={Date.now()} data={trackGeojson} />
+        {hasDataValues && (
+          <Pane name="point-circles" style={{ zIndex: 899 }}>
+            {pointsByTime.map(({ position, value }, i) => (
+              <CircleMarker
+                key={i}
+                center={position}
+                color={interpolateRdYlBu(Math.min(1, (value || 10) / 10))}
+                radius={1}
+              >
+                <Tooltip>{value?.toFixed(1)}</Tooltip>
+              </CircleMarker>
+            ))}
+          </Pane>
+        )}
+        <Pane name="current-point" style={{ zIndex: 999 }}>
+          {this.state.currentPoint ? <CircleMarker center={this.state.currentPoint} radius={5} /> : undefined}
+        </Pane>
+      </MapContainer>
+    );
+  }
+}
+
+const toTileLayer = ({ url, minZoom, maxZoom, maxNativeZoom, subdomains }: TileLayerProps) => (
+  <TileLayer url={url} minZoom={minZoom} maxZoom={maxZoom} maxNativeZoom={maxZoom} subdomains={subdomains} />
+);
+
+const dataToMapParams = memoize((data: any): MapParams => {
+  const pointsByTime: PositionWithTime[] = [];
+  const trackGeojson: LineString = {
+    type: 'LineString',
+    coordinates: [],
+  };
+
+  let i;
+  const positions = data.series[0].fields[1].values;
+  const timestamps = data.series[0].fields[0].values;
+  const dataValues = data.series[0].fields.length > 2 ? data.series[0].fields[2].values : undefined;
+  let minLat = 90;
+  let maxLat = -90;
+  let minLng = 180;
+  let maxLng = -180;
+  const validatingBuffer = new ValidatingBuffer<any>(
+    (points: any[]) => {
+      if (points.length === 3 && !points[1]) {
+        return false;
+      }
+      const firstSecondDiff = Math.abs(points[0][1] + points[0][1] - points[1][0] - points[1][1]);
+      const firstLastDiff = Math.abs(points[0][1] + points[0][1] - points[2][0] - points[2][1]);
+      const secondLastDiff = Math.abs(points[1][1] + points[1][1] - points[2][0] - points[2][1]);
+      const threshold = firstLastDiff * 2;
+      const isValid = firstSecondDiff < threshold && secondLastDiff < threshold;
+      return isValid;
+    },
+    (point: any) => {
+      minLat = Math.min(minLat, point[1]);
+      minLng = Math.min(minLng, point[0]);
+      maxLat = Math.max(maxLat, point[1]);
+      maxLng = Math.max(maxLng, point[0]);
+      trackGeojson.coordinates.push(point);
+    }
+  );
+  for (i = 0; i < positions.length; i++) {
+    const position = positions.get(i);
+    if (
+      position !== null &&
+      Array.isArray([position]) &&
+      typeof position[0] === 'number' &&
+      typeof position[1] === 'number'
+    ) {
+      const timePoint: PositionWithTime = {
+        timestamp: timestamps.get(i),
+        position: new LatLng(position[1], position[0]),
+      };
+      if (dataValues) {
+        timePoint.value = dataValues.get(i);
+      }
+      pointsByTime.push(timePoint);
+      validatingBuffer.push(position);
+    }
+  }
+  validatingBuffer.flush();
+
+  const bounds = new LatLngBounds([minLat, minLng], [maxLat, maxLng]);
+
+  return {
+    trackGeojson,
+    bounds,
+    pointsByTime,
+    getTimeframeByBounds: timeframeByBoundsGetter(pointsByTime),
+    hasDataValues: !!dataValues,
+  };
+});
+
+function timeframeByBoundsGetter(pointsByTime: PositionWithTime[]) {
+  return (bounds: LatLngBounds): AbsoluteTimeRange | null => {
+    const timerange = pointsByTime.reduce<AbsoluteTimeRange>(
+      (acc, { position, timestamp }) => {
+        if (bounds.contains(position)) {
+          return { from: Math.min(acc.from, timestamp), to: Math.max(acc.to, timestamp) };
+        }
+        return acc;
+      },
+      { from: Number.POSITIVE_INFINITY, to: 0 }
+    );
+
+    if (timerange.from <= timerange.to) {
+      return timerange;
+    }
+    return null;
+  };
+}
--- trackmap_g10/src/module.ts
+++ trackmap_g10/src/module.ts
@@ -1,40 +1,16 @@
 import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { SimplePanel } from './components/SimplePanel';
+import { TrackMapOptions } from './types';
+import { TrackMapPanel } from './TrackMapPanel';
 
-export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
-  return builder
-    .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
-    })
-    .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
-      defaultValue: false,
-    })
-    .addRadio({
-      path: 'seriesCountSize',
-      defaultValue: 'sm',
-      name: 'Series counter size',
-      settings: {
-        options: [
-          {
-            value: 'sm',
-            label: 'Small',
-          },
-          {
-            value: 'md',
-            label: 'Medium',
-          },
-          {
-            value: 'lg',
-            label: 'Large',
-          },
-        ],
-      },
-      showIf: (config) => config.showSeriesCount,
-    });
+export const plugin = new PanelPlugin<TrackMapOptions>(TrackMapPanel).setPanelOptions((builder) => {
+  return builder.addColorPicker({
+    path: 'trackColor',
+    name: 'Track color',
+    defaultValue: '#0000ff'
+  }).addColorPicker({
+    path: 'pointColor',
+    name: 'Point color',
+    defaultValue: '#ff0000'
+  })
 });
+
